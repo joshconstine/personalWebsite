cdcfec0f20a5447b574dc66fcdfa3314847b795e
joshua constine <joshua.constine97@gmail.com>
2022-04-07 10:37:51 -0500

    done

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
-    <h1>Joshua Constine</h1>
+    <h1>Joshua Constin!!</h1>
     <nav>
       <div>
         {/* The navbar will show these links after you log in */}
